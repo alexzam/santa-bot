5d6a65a8c32477e18c055fa98972a268b9b3a6ff
Alexander Zamyshlyaev <zamysh@yandex-team.ru>
2018-11-13 17:59:15 +0300

    DB connection

--- src/main/kotlin/az/santabot/DbService.kt
+++ src/main/kotlin/az/santabot/DbService.kt
@@ -1,5 +1,18 @@
 package az.santabot
 
+import java.sql.Connection
+import java.sql.DriverManager
+
+
 class DbService {
+    private val dbUrl = System.getenv("JDBC_DATABASE_URL")
+
     fun getUrlInfo(): String = "JDU:${System.getenv("JDBC_DATABASE_URL")}, DU:${System.getenv("DATABASE_URL")}"
+
+    private fun <T : Any> withConnection(action: Connection.() -> T): T {
+        val connection = DriverManager.getConnection(dbUrl)!!
+        val ret = connection.action()
+        connection.close()
+        return ret
+    }
 }
