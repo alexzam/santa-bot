c76d0e2566de5b8d721ac9f26cb387c805939ad5
Alexander Zamyshlyaev <zamysh@yandex-team.ru>
2018-11-15 21:31:16 +0300

    Fix(?) message sending

--- src/main/kotlin/az/santabot/model/SendMessageRequest.kt
+++ src/main/kotlin/az/santabot/model/SendMessageRequest.kt
@@ -1,8 +1,10 @@
 package az.santabot.model
 
 import az.santabot.util.Either
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class SendMessageRequest(
     @JsonProperty("chat_id") val chatId: Either<Int, String>,
     val text: String,
