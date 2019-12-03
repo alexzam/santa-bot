cbfa4eb567616fd135dc2b13b27d2b312c4f57ed
Alexander Zamyshlyaev <zamysh@yandex-team.ru>
2019-12-04 01:31:09 +0300

    Fix possible null usernames

--- src/main/kotlin/az/santabot/DbService.kt
+++ src/main/kotlin/az/santabot/DbService.kt
@@ -241,7 +241,10 @@ class Group(results: ResultSet) {
 
 class DbUser(results: ResultSet) {
     private val name: String? = results.getString("target_name")
-    private val username: String = results.getString("target_username")
+    private val username: String? = results.getString("target_username")
 
-    override fun toString(): String = if (name != null) "$name (@$username)" else "@$username"
+    override fun toString(): String =
+        if (name != null && username != null) "$name (@$username)"
+        else if (username != null) "@$username"
+        else name ?: "Fully anonymous user"
 }
