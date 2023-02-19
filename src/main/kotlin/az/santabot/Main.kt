db34daacb65a800f43591c409064a674409bb563
Alexander Zamyshlyaev <alexzzam@gmail.com>
2023-02-19 21:53:24 +0200

    Update some libs and Gradle itself

--- src/main/kotlin/az/santabot/Main.kt
+++ src/main/kotlin/az/santabot/Main.kt
@@ -1,64 +1,60 @@
 package az.santabot
 
 import az.santabot.model.Update
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.ktor.application.*
-import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.jackson.*
-import io.ktor.request.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 import kotlin.random.nextUBytes
 
-@ExperimentalUnsignedTypes
-fun main(args: Array<String>) {
+fun main() {
+    embeddedServer(Netty, System.getenv("PORT").toIntOrNull() ?: 80, module = Application::santaBotModule)
+        .start(wait = true)
+}
 
-    val immediateResponseMode = false
-
+@OptIn(ExperimentalUnsignedTypes::class)
+fun Application.santaBotModule() {
     // DI for tiny app
     val incomingToken = Random.nextUBytes(10).map { it.toString(16) }.fold("") { acc, s -> acc + s }
+    val immediateResponseMode = false
+
     val dbService = DbService()
+
     val santaService = SantaService(dbService)
     val telegramService = TelegramService(incomingToken, santaService)
     santaService.telegramService = telegramService
-    val jacksonObjectMapper = jacksonObjectMapper()
 
     runBlocking { println("Telegram endpoint setup ($incomingToken): " + telegramService.setupEndpoint()) }
 
-    embeddedServer(Netty, System.getenv("PORT").toIntOrNull() ?: 80) {
-        install(ContentNegotiation) {
-            jackson {}
-        }
-        routing {
-            get("/") {
-                call.respondText(dbService.getUrlInfo(), ContentType.Text.Html)
-            }
-            post("/tg/$incomingToken") {
-                val update: Update
-                try {
-                    val receiveText = call.receiveText()
-                    println("IN: $receiveText")
-                    update = jacksonObjectMapper.readValue(receiveText, Update::class.java)
-                    val response = telegramService.onReceiveUpdate(update)
-                    val responseStr = jacksonObjectMapper.writeValueAsString(response)
+    install(ContentNegotiation) {
+        jackson()
+    }
 
-                    if (immediateResponseMode) {
-                        println("OUT: $responseStr")
-                        call.respond(response ?: "")
-                    } else {
-                        call.respond(HttpStatusCode.OK, "")
-                        response?.also { telegramService.sendRequest(it) }
-                    }
-                } catch (e: Exception) {
-                    println(e)
-                    e.printStackTrace()
+    routing {
+        get("/") {
+            call.respondText(dbService.getUrlInfo(), ContentType.Text.Html)
+        }
+        post("/tg/$incomingToken") {
+            try {
+                val update = call.receive<Update>()
+                val response = telegramService.onReceiveUpdate(update)
+
+                if (immediateResponseMode) {
+                    call.respond(response ?: "")
+                } else {
+                    call.respond(HttpStatusCode.OK, "")
+                    response?.also { telegramService.sendRequest(it) }
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
-    }.start(wait = true)
+    }
 }
